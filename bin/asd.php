cea3a790b83840b971639e65e00395ceb73c4333
Akihito Koriyama <akihito.koriyama@gmail.com>
2019-12-19 04:43:38 +0900

    add bin command

---
+++ bin/asd.php
@@ -0,0 +1,24 @@
+#!/usr/bin/env php
+<?php
+
+declare(strict_types=1);
+
+use Koriym\AlpsStateDiagram\AlpsStateDiagram;
+
+foreach ([__DIR__ . '/../../../autoload.php', __DIR__ . '/../vendor/autoload.php'] as $file) {
+    if (file_exists($file)) {
+        require $file;
+        break;
+    }
+}
+
+if ($argc !== 2) {
+    echo 'usage: asd <alps>' . PHP_EOL;
+    exit(1);
+}
+[, $profile] = $argv;
+$dot = (new AlpsStateDiagram)($profile);
+var_dump($dot);
+
+$outputfile = $profile . '.dot';
+file_put_contents($outputfile, $dot);
