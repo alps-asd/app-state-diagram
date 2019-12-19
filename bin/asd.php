79c1d4e3d10d7e9fa90e0aceeaca8900a0963d96
Akihito Koriyama <akihito.koriyama@gmail.com>
2019-12-19 11:59:14 +0900

    fix cs

--- bin/asd.php
+++ bin/asd.php
@@ -8,6 +8,7 @@ use Koriym\AlpsStateDiagram\AlpsStateDiagram;
 foreach ([__DIR__ . '/../../../autoload.php', __DIR__ . '/../vendor/autoload.php'] as $file) {
     if (file_exists($file)) {
         require $file;
+
         break;
     }
 }
