066999a4322b64ad3b7a99601df5e38c4b67c407
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-11-16 22:16:11 +0900

    Update AppStateDiagram.php

--- src/AppStateDiagram.php
+++ src/AppStateDiagram.php
@@ -4,20 +4,14 @@ declare(strict_types=1);
 
 namespace Koriym\AppStateDiagram;
 
-use function basename;
-
 final class AppStateDiagram
 {
     /** @var AlpsProfile */
     private $alps;
 
-    /** @var string */
-    private $alpsFile;
-
     public function __construct(string $alpsFile)
     {
         $this->alps = new AlpsProfile($alpsFile);
-        $this->alpsFile = basename($alpsFile);
     }
 
     public function getDot(): string
