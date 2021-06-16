43256946f92ef2e3f7f88698bcb79dc287c0ccba
tsuchiya <tsuchiya@bengo4.com>
2021-06-16 22:29:04 +0900

    Add red tests

--- tests/ConfigLoadTest.php
+++ tests/ConfigLoadTest.php
@@ -6,6 +6,7 @@ namespace Koriym\AppStateDiagram;
 
 use PHPUnit\Framework\TestCase;
 
+use function count;
 use function file_exists;
 
 class ConfigLoadTest extends TestCase
@@ -18,5 +19,46 @@ class ConfigLoadTest extends TestCase
         $this->assertFalse($config->watch);
         $this->assertSame(['tag1', 'tag2'], $config->filter->and);
         $this->assertSame(['tag3'], $config->filter->or);
+    }
+
+    /**
+     * @return list<string>
+     */
+    public function testOverwriteConfig(): array
+    {
+        $argv = [
+            '--watch',
+            '--and-tag=a,b',
+            '--or-tag=c,d',
+            '--color=red',
+            '-c',
+            __DIR__ . '/Fake/config',
+            __DIR__ . '/Fake/alps.json',
+        ];
+        $config = ConfigFactory::fromFile(__DIR__ . '/Fake/config', count($argv), $argv);
+        $this->assertSame(__DIR__ . '/Fake/alps.json', $config->profile);
+        $this->assertTrue($config->watch);
+        $this->assertTrue($config->hasTag);
+        $this->assertSame(['a', 'b'], $config->filter->and);
+        $this->assertSame(['c', 'd'], $config->filter->or);
+        $this->assertSame('red', $config->filter->color);
+
+        return $argv;
+    }
+
+    /**
+     * @param list<string> $argv
+     *
+     * @depends testOverwriteConfig
+     */
+    public function testFromCommandLine(array $argv): void
+    {
+        $config = ConfigFactory::fromCommandLine(count($argv), $argv);
+        $this->assertSame(__DIR__ . '/Fake/alps.json', $config->profile);
+        $this->assertTrue($config->watch);
+        $this->assertTrue($config->hasTag);
+        $this->assertSame(['a', 'b'], $config->filter->and);
+        $this->assertSame(['c', 'd'], $config->filter->or);
+        $this->assertSame('red', $config->filter->color);
     }
 }
