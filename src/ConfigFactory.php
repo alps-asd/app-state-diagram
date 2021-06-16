c833213d6459187067e314a7486ee9ae9007c218
tsuchiya <tsuchiya@bengo4.com>
2021-06-16 22:30:10 +0900

    Add config override with cli options

--- src/ConfigFactory.php
+++ src/ConfigFactory.php
@@ -9,11 +9,14 @@ use SimpleXMLElement;
 
 use function assert;
 use function dirname;
+use function explode;
+use function in_array;
 use function is_dir;
 use function is_file;
 use function property_exists;
 use function realpath;
 use function sprintf;
+use function strpos;
 
 final class ConfigFactory
 {
@@ -28,23 +31,84 @@ final class ConfigFactory
 
         $maybePath = (string) realpath($argv[$argc - 1]);
         $profile = is_file($maybePath) ? $maybePath : sprintf('%s/%s', $dir, (string) $xml->alpsFile);
+        $watch = in_array('--watch', $argv, true) || (string) $xml->watch === 'true';
         /** @var ?SimpleXMLElement $filter */
-        $filter = property_exists($xml, 'filter') ?  $xml->filter : null;
+        $filter = property_exists($xml, 'filter') ? $xml->filter : null;
+        $and = self::parseAndTag($argv, $filter);
+        $or = self::parseOrTag($argv, $filter);
+        $color = self::parseColor($argv, $filter);
 
         return new Config(
             $profile,
-            (string) $xml->watch === 'true',
-            $filter
+            $watch,
+            new ConfigFilter($and, $or, $color)
         );
     }
 
     /** @param array<string> $argv */
     public static function fromCommandLine(int $argc, array $argv): Config
     {
+        $watch = in_array('--watch', $argv, true);
+        $and = self::parseAndTag($argv, null);
+        $or = self::parseOrTag($argv, null);
+        $color = self::parseColor($argv, null);
+
         return new Config(
             (string) realpath($argv[$argc - 1]),
-            false,
-            null
+            $watch,
+            new ConfigFilter($and, $or, $color)
         );
+    }
+
+    /**
+     * @param array<string> $options
+     *
+     * @return array<string>
+     */
+    private static function parseAndTag(array $options, ?SimpleXMLElement $filter): array
+    {
+        foreach ($options as $option) {
+            if (strpos($option, '--and-tag=') === 0) {
+                [, $value] = explode('=', $option);
+
+                return explode(',', $value);
+            }
+        }
+
+        return $filter instanceof SimpleXMLElement && property_exists($filter, 'and') ? (array) $filter->and : [];
+    }
+
+    /**
+     * @param array<string> $options
+     *
+     * @return array<string>
+     */
+    private static function parseOrTag(array $options, ?SimpleXMLElement $filter): array
+    {
+        foreach ($options as $option) {
+            if (strpos($option, '--or-tag=') === 0) {
+                [, $value] = explode('=', $option);
+
+                return explode(',', $value);
+            }
+        }
+
+        return $filter instanceof SimpleXMLElement && property_exists($filter, 'or') ? (array) $filter->or : [];
+    }
+
+    /**
+     * @param array<string> $options
+     */
+    private static function parseColor(array $options, ?SimpleXMLElement $filter): string
+    {
+        foreach ($options as $option) {
+            if (strpos($option, '--color=') === 0) {
+                [, $value] = explode('=', $option);
+
+                return $value;
+            }
+        }
+
+        return $filter instanceof SimpleXMLElement && property_exists($filter, 'color') ? (string) $filter->color : '';
     }
 }
