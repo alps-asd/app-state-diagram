0d31e2654b7f8674470f4cf4f0103bb4cb197ee4
Akihito Koriyama <akihito.koriyama@gmail.com>
2021-11-22 00:46:24 +0900

    CI suspended when ALPS is invalid
    https://github.com/koriym/asd-action/pull/11

--- docker/asd-action/entrypoint.sh
+++ docker/asd-action/entrypoint.sh
@@ -2,5 +2,10 @@
 
 dirname=$(dirname $2)
 /vendor/bin/asd --config=$1 $2
-echo "Output Directory: $dirname"
-echo "::set-output name=dir::$dirname"
+if [ $? -ne "0" ]; then
+  echo "ASD error"
+  exit 1
+else
+  echo "Output Directory: $dirname"
+  echo "::set-output name=dir::$dirname"
+fi
