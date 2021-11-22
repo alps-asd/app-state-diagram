083e6e531047494c07441c269899daaef9e5ec50
Akihito Koriyama <akihito.koriyama@gmail.com>
2021-11-22 10:57:01 +0900

    Fix wrong asd image name

--- docs/asd.sh
+++ docs/asd.sh
@@ -14,4 +14,4 @@ profile=$(cd "$(dirname "$target")" || exit; pwd)/$(basename "$target") # path/t
 dir=$(dirname "$profile") # path/to
 basename=$(basename "$profile") # profile.xml
 
-docker run -v "$dir:/work" -it --init --rm --name asd -p 3000:3000 app-state-diagram:latest composer global exec asd -- "$options" /work/"$basename"
+docker run -v "$dir:/work" -it --init --rm --name asd -p 3000:3000 ghcr.io/koriym/app-state-diagram composer global exec asd -- "$options" /work/"$basename"
