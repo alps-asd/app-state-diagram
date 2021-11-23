8999b1a3bd08145bb32f55fcb5e371e2f6a848c2
tsuchiya <tsuchiya@bengo4.com>
2021-11-23 21:17:01 +0900

    Add script for running asd-action container

--- docker/asd-action/build.sh
+++ docker/asd-action/build.sh
@@ -2,5 +2,5 @@
 
 cd "$(dirname "$0")"/.. || exit
 
-docker build -t asd-action:latest -f asd-action/Dockerfile asd-action
+docker build -t ghcr.io/alps-asd/asd-action:latest -f asd-action/Dockerfile asd-action
 # docker scan asd-action:latest
