293bcf283958950c3031cb0774617f001cb14e3e
Akihito Koriyama <akihito.koriyama@gmail.com>
2022-07-21 08:40:45 +0900

    Build multi-architecture images with buildx in local build script

--- docker/app-state-diagram/build.sh
+++ docker/app-state-diagram/build.sh
@@ -2,5 +2,5 @@
 
 cd "$(dirname "$0")"/.. || exit
 
-docker build -t ghcr.io/alps-asd/app-state-diagram:latest -f app-state-diagram/Dockerfile app-state-diagram
+docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/alps-asd/app-state-diagram:latest -f app-state-diagram/Dockerfile app-state-diagram
 # docker scan app-state-diagram:latest
