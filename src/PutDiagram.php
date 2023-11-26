d4a5a5da0172e0671d3a7aa41a2d157530af7fa6
Akihito Koriyama <akihito.koriyama@gmail.com>
2023-11-27 02:34:31 +0900

    Output svg using viz.js only when markdown

--- src/PutDiagram.php
+++ src/PutDiagram.php
@@ -5,19 +5,54 @@ declare(strict_types=1);
 namespace Koriym\AppStateDiagram;
 
 use function count;
+use function dirname;
 use function file_put_contents;
+use function passthru;
 use function sprintf;
+use function str_replace;
 
 use const PHP_EOL;
 
 final class PutDiagram
 {
+    /** @var DrawDiagram */
+    private $draw;
+
+    public function __construct()
+    {
+        $this->draw = new DrawDiagram();
+    }
+
     public function __invoke(Config $config): void
     {
         $profile = new Profile($config->profile, new LabelName());
         $index = new IndexPage($config);
+        if ($config->outputMode === DumpDocs::MODE_MARKDOWN) {
+            $titleProfile = new Profile($config->profile, new LabelNameTitle());
+            $this->draw('', new LabelName(), $profile, null, null);
+            $this->draw('.title', new LabelNameTitle(), $titleProfile, null, null);
+        }
+
         file_put_contents($index->file, $index->content);
         echo "ASD generated. {$index->file}" . PHP_EOL;
         echo sprintf('Descriptors(%s), Links(%s)', count($profile->descriptors), count($profile->links)) . PHP_EOL;
+    }
+
+    private function draw(string $fileId, LabelNameInterface $labelName, AbstractProfile $profile, ?TaggedProfile $taggedProfile, ?string $color): void
+    {
+        $dot = ($this->draw)($profile, $labelName, $taggedProfile, $color);
+        $extention = $fileId . '.dot';
+        $dotFile = str_replace(['.xml', '.json'], $extention, $profile->alpsFile);
+        $this->convert($dotFile, $dot);
+    }
+
+    private function convert(string $dotFile, string $dot): void
+    {
+        file_put_contents($dotFile, $dot);
+        $cmd = sprintf('node %s %s', dirname(__DIR__) . '/asd-sync/dot.js', $dotFile);
+        passthru($cmd, $status);
+        if ($status !== 0) {
+            echo 'Warning: Graphviz error' . PHP_EOL; // @codeCoverageIgnore
+        }
     }
 }
